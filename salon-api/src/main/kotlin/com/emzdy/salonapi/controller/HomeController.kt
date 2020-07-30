9b71b8ba05bf3edbaa8dde6b75cdba60bffef177
ladislavlisy <ladislav.lisy@seznam.cz>
2020-07-30 15:13:36 +0200

    Repository Postgres

--- salon-api/src/main/kotlin/com/emzdy/salonapi/controller/HomeController.kt
+++ salon-api/src/main/kotlin/com/emzdy/salonapi/controller/HomeController.kt
@@ -4,10 +4,15 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 import com.emzdy.salonapi.config.SalonDetails
+import com.emzdy.salonapi.repository.SalonServiceDetailRepository
 
 @RestController
-class HomeController(private val homeDetails: SalonDetails) {
+class HomeController(private val rep: SalonServiceDetailRepository, private val homeDetails: SalonDetails) {
 
     @RequestMapping(value = ["/"], method = [RequestMethod.GET])
-    fun getHome() = "<h1>Hello from: " + homeDetails.name + "</h1>"
+    fun getHome(): String {
+        val detailsCount : Long = rep.count()
+        val detailsName = homeDetails.name
+        return "<h1>Hello from: $detailsName</h1>\nSalon Services Count: $detailsCount"
+    }
 }
